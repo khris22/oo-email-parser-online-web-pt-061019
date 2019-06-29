6064f91d387814ca1f76b5634b0a60102a40b021
Khris Punzalan <khrispunzalan@gmail.com>
2019-06-29 22:40:43 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -22,7 +22,7 @@ class EmailParser
   def parse
     # @emails = emails.split (/[,\s+]/)
     @emails.split(/[,\s]/)
-    @emails.to_s.delete!([""])
+    @emails.delete!([""])
     # {|string| string.empty?}
     @emails.uniq
     # binding.pry
